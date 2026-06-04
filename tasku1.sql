2ed589a698ac46e4143cff3a09a4b761afa2a2c3
NailaZahra <nailazahray@gmail.com>
2026-06-04 18:49:40 +0700

    perubahan sedikit di dashboard dan tasku.sql

--- tasku1.sql
+++ tasku1.sql
@@ -53,7 +53,7 @@ CREATE TABLE `user` (
   `email` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
